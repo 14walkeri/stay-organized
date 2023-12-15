bf98014185719c2496225829cd162bf2187dc645
Ian Walker <14walkeri@comcast.net>
2023-12-15 13:51:06 -0500

    getting todos to display in cards

--- scripts/todos.js
+++ scripts/todos.js
@@ -1,7 +1,7 @@
 // getting html elements for the boys
 let userSelect = document.querySelector("#user-select");
-
-
+let todoCards = document.querySelector("#todo-card");
+let divDisplay = document.querySelector("#display");
 // function to show the boys
 function loadTodoDrop() {
   fetch("http://localhost:8083/api/users")
@@ -9,14 +9,50 @@ function loadTodoDrop() {
     .then((data) => {
       for (const user of data) {
         let userOption = document.createElement("option");
-        userOption.innerText = user.username;
+        userOption.innerText = user.name;
         userOption.value = user.id;
         userSelect.appendChild(userOption);
       }
     });
 }
 
+function displayTodos() {
+  fetch(`http://localhost:8083/api/todos/byuser/${userSelect.value}`)
+    .then((response) => response.json())
+    .then((todos) => {
+      for (let todo of todos) {
+        if (todoCards == true) {
+          todoCards.removeChild(todoCards);
+          buildACard(todo);
+        } else {
+          buildACard(todo);
+        }
+      }
+    });
+}
+// getting my cards to show function
+function buildACard(todo) {
+  let currentCard = document.createElement("div");
 
+  let currentH3 = document.createElement("h3");
+  let currentH5 = document.createElement("h4");
+  let currentP = document.createElement("h5");
+  let currentP2 = document.createElement("p");
 
+  currentCard.classList.add("card");
+
+  currentH3.innerText = todo.category;
+  currentH5.innerText = todo.description;
+  currentP.innerText = todo.deadline;
+  currentP2.innerText = `Piority:${todo.priority}\nCompleted:${todo.completed}`;
+
+  currentCard.appendChild(currentH3);
+  currentCard.appendChild(currentH5);
+  currentCard.appendChild(currentP);
+  currentCard.appendChild(currentP2);
+
+  todoCards.appendChild(currentCard);
+}
 // wiring it up for the boys
 window.onload = loadTodoDrop;
+userSelect.onchange = displayTodos;
